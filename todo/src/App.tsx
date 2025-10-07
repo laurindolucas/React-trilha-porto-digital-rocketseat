d50ebda9835c29bbe376a5ad763e704117d1dca1
laurindolucas <caiolucax2024@gmail.com>
2025-10-07 14:13:10 -0300

    feat: adicionando o componente de botão e sua configuração de estilos

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,13 +1,15 @@
 import Text from "./components/text";
 import Icon from "./components/icon"
+import Badge from "./components/badge";
+import Button from "./components/button";
+
 import TrashIcon from "./assets/icons/Trash-Regular.svg?react";
 import CheckIcon from "./assets/icons/Check-Regular.svg?react"
-
 import XIcon from "./assets/icons/X-Regular.svg?react";
 import PencilIcon from "./assets/icons/PencilSimple-Regular.svg?react";
 import PlusIcon from "./assets/icons/Plus-Regular.svg?react";
 import SpinnerIcon from "./assets/icons/spinner.svg?react";
-import Badge from "./components/badge";
+
 export default function App() {
 
   return (
@@ -30,6 +32,10 @@ export default function App() {
       <div>
         <Badge variant="secondary">5</Badge>
         <Badge variant="primary">2 de 5</Badge>
+      </div>
+
+      <div>
+        <Button icon={PlusIcon}>Nova Tarefa</Button>
       </div>
 
      
