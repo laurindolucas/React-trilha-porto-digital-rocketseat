5f404d3915b43ecd292340bcee481d3371fd2519
laurindolucas <caiolucax2024@gmail.com>
2025-10-07 11:32:22 -0300

    feat: crianção dos badges

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -7,6 +7,7 @@ import XIcon from "./assets/icons/X-Regular.svg?react";
 import PencilIcon from "./assets/icons/PencilSimple-Regular.svg?react";
 import PlusIcon from "./assets/icons/Plus-Regular.svg?react";
 import SpinnerIcon from "./assets/icons/spinner.svg?react";
+import Badge from "./components/badge";
 export default function App() {
 
   return (
@@ -24,6 +25,11 @@ export default function App() {
         <Icon svg={SpinnerIcon} animate />
         <Icon svg={PencilIcon} />
         <Icon svg={XIcon} />
+      </div>
+
+      <div>
+        <Badge variant="secondary">5</Badge>
+        <Badge variant="primary">2 de 5</Badge>
       </div>
 
      
