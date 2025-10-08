5239377d4de60393ba79f02cc3e93403ad8e2c0a
laurindolucas <caiolucax2024@gmail.com>
2025-10-08 11:41:25 -0300

    feat: adicionando roteamento entre as páginas

---
+++ todo/src/pages/page-components.tsx
@@ -0,0 +1,67 @@
+import Text from "../components/text";
+import Icon from "../components/icon"
+import Badge from "../components/badge";
+import Button from "../components/button";
+import ButtonIcon from "../components/button-icon";
+import InputText from "../components/input-text";
+import InputCheckbox from "../components/input-checkbox";
+import Card from "../components/card";
+import Container from "../components/container";
+
+import TrashIcon from "../assets/icons/Trash-Regular.svg?react";
+import CheckIcon from "../assets/icons/Check-Regular.svg?react"
+import XIcon from "../assets/icons/X-Regular.svg?react";
+import PencilIcon from "../assets/icons/PencilSimple-Regular.svg?react";
+import PlusIcon from "../assets/icons/Plus-Regular.svg?react";
+import SpinnerIcon from "../assets/icons/spinner.svg?react";
+
+export default function PageComponents(){
+    return (
+    <Container>
+      <div className="grid gap-10">
+        <div className="flex flex-col gap-1">
+          <Text variant="body-sm-bold" className="text-pink-base"> Ola mundo!</Text>
+          <Text variant="body-sm-bold" className="text-green-base"> Ola mundo!</Text>
+          <Text variant="body-sm-bold" > Ola mundo!</Text>
+        </div>
+
+        <div className="flex gap-x-1">
+          <Icon svg={TrashIcon} className="fill-green-base" />
+          <Icon svg={CheckIcon} />
+          <Icon svg={PlusIcon} />
+          <Icon svg={SpinnerIcon} animate />
+          <Icon svg={PencilIcon} />
+          <Icon svg={XIcon} />
+        </div>
+
+        <div className="flex gap-1">
+          <Badge variant="secondary">5</Badge>
+          <Badge loading>2 de 5</Badge>
+        </div>
+
+        <div>
+          <Button icon={PlusIcon}>Nova Tarefa</Button>
+        </div>
+
+        <div className="flex gap-1">
+          <ButtonIcon icon={TrashIcon} disabled />
+          <ButtonIcon icon={TrashIcon} variant="secondary" />
+          <ButtonIcon icon={TrashIcon} variant="tertiary" loading />
+        </div>
+        <div>
+          <InputText /> 
+        
+        </div>
+        <div>
+          <InputCheckbox loading />
+        </div>
+        <div>
+          <Card size="md">Olá mundo</Card>
+        </div>
+
+
+
+      </div>
+    </Container>
+  )
+}
---
+++ todo/src/pages/page-home.tsx
@@ -0,0 +1,5 @@
+ 
+
+ export default function PageHome(){
+    return <>Pagina home</>
+ }
