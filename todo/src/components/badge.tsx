77d00690aeef74d36100ddbb3161b8e4a5622ec8
laurindolucas <caiolucax2024@gmail.com>
2025-10-08 11:01:40 -0300

    refactor: atualizando os componentes badge, button-icon e input-checkbox com animações do skeleton

--- todo/src/components/badge.tsx
+++ todo/src/components/badge.tsx
@@ -1,43 +1,72 @@
 import React from "react";
 import Text from "./text"
-import {cva, type VariantProps} from "class-variance-authority";
+import { cva,cx, type VariantProps } from "class-variance-authority";
+import Skeleton from "./skeleton";
 
-export const badgeVariants = cva("inline-flex items-center justify-center rounded-full",{
-    variants:{
+export const badgeVariants = cva("inline-flex items-center justify-center rounded-full", {
+    variants: {
         variant: {
+              none: "",
             primary: "bg-green-light",
             secondary: "bg-pink-light"
         },
-        size:{
+        size: {
             sm: "py-0.5 px-2"
         }
     },
-    defaultVariants:{
+    defaultVariants: {
         variant: "primary",
         size: "sm"
     }
-    });
+});
 
-    export const badgeTextVariants = cva("", {
-       variants: {
+export const badgeTextVariants = cva("", {
+    variants: {
         variant: {
+            none: "",
             primary: "text-green-dark",
             secondary: "text-pink-dark"
 
         }
-       },
-       defaultVariants:{
-        variant:"primary"
-       }
-    })
-interface BadgeProps extends React.ComponentProps<"div">, VariantProps<typeof badgeVariants>{
+    },
+    defaultVariants: {
+        variant: "primary"
+    }
+});
 
+export const badgeSkeletonVariants = cva("",
+    {
+        variants: {
+            size: {
+                sm: "w-6 h-6"
+            }
+        }, defaultVariants: {
+            size: "sm"
+        }
+
+    })
+
+
+
+interface BadgeProps extends React.ComponentProps<"div">, VariantProps<typeof badgeVariants> {
+    loading?: boolean;
 }
 
-export default function Badge({ variant, size, className, children, ...props }: BadgeProps) {
+export default function Badge({ variant, size, className, children, loading, ...props }: BadgeProps) {
+    if (loading) {
+        return (<Skeleton rounded="full"
+            className={cx(badgeVariants({ variant: "none" }),
+                badgeSkeletonVariants({ size }),
+                className
+            )}
+        />
+        )
+    }
+
+
     return (
-        <div className={badgeVariants({variant, size, className})} {...props}>
-            <Text variant="body-sm-bold" className={badgeTextVariants({variant})}>{children}</Text>
+        <div className={badgeVariants({ variant, size, className })} {...props}>
+            <Text variant="body-sm-bold" className={badgeTextVariants({ variant })}>{children}</Text>
         </div>
     );
 }
--- todo/src/components/input-checkbox.tsx
+++ todo/src/components/input-checkbox.tsx
@@ -2,31 +2,37 @@ import { type VariantProps, cva } from "class-variance-authority";
 import React from "react";
 import CheckIcon from "../assets/icons/Check-Regular.svg?react";
 import Icon from "./icon";
-
+import Skeleton from "./skeleton";
 export const inputCheckboxWrapperVariants = cva(` inline-flex items-center justify-center  relative group`);
 
 export const inputCheckboxVariants = cva(`
     appearance-none peer flex items-center justify-center cursor-pointer
-  border-2 border-solid transition overflow-hidden border-green-base
-  hover:border-green-dark hover:bg-green-dark/20
-  checked:border-green-base checked:bg-green-base
-  group-hover:checked:border-green-dark group-hover:checked:bg-green-dark
-
+  transition overflow-hidden border-green-base
+ 
     `,
-    {
-        variants:{
-            size:{
-                md: 'w-5 h-5 rounded-sm'
-            },
-            disabled:{
-                true: "pointer-evenst-none"
-            }
-        },
-        defaultVariants:{
-            size: "md",
-            disabled: false
-        }
+  {
+    variants: {
+      variant: {
+        none: " ",
+        default: ` border-2 border-solid
+             hover:border-green-dark hover:bg-green-dark/20
+             checked:border-green-base checked:bg-green-base
+             group-hover:checked:border-green-dark group-hover:checked:bg-green-dark
+           `
+      },
+      size: {
+        md: 'w-5 h-5 rounded-sm'
+      },
+      disabled: {
+        true: "pointer-evenst-none"
+      }
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "md",
+      disabled: false
     }
+  }
 );
 
 
@@ -48,16 +54,25 @@ export const inputCheckboxIconVariants = cva(
 );
 
 interface InputCheckboxProps extends VariantProps<typeof inputCheckboxVariants>,
-Omit<React.ComponentProps<"input">, "size" | "disabled">{}
+  Omit<React.ComponentProps<"input">, "size" | "disabled"> { loading?: boolean; }
 
-export default function InputCheckbox({size, disabled, className, ...props}: InputCheckboxProps){
-    return ( 
-    <label className={inputCheckboxWrapperVariants({className})}>
-        <input type="checkbox" 
-        className={inputCheckboxVariants({size, disabled})} 
+export default function InputCheckbox({variant, size, disabled, className, loading, ...props }: InputCheckboxProps) {
+  if (loading) {
+    return (
+      <Skeleton
+        rounded="sm"
+        className={inputCheckboxVariants({variant:"none", size})}
+      />
+    );
+  }
+
+  return (
+    <label className={inputCheckboxWrapperVariants({ className })}>
+      <input type="checkbox"
+        className={inputCheckboxVariants({variant, size, disabled })}
         {...props} />
-        <Icon className={inputCheckboxIconVariants({size})} 
+      <Icon className={inputCheckboxIconVariants({ size })}
         svg={CheckIcon} />
     </label>
-    );
+  );
 }
